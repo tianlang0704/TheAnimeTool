830dde92d1e93ef76896f553496ead5082ae533d
Tieria C.Monk <tianlang0704@tierias-mac-2.local>
2016-08-27 08:47:00 -0700

    Initial Commit

---
+++ TheAnimeToolTests/TheAnimeToolTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheAnimeToolTests.swift
+//  TheAnimeToolTests
+//
+//  Created by Tieria C.Monk on 8/27/16.
+//  Copyright © 2016 Tieria C.Monk. All rights reserved.
+//
+
+import XCTest
+@testable import TheAnimeTool
+
+class TheAnimeToolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
